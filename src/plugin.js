406ddaff05941152b090202198fa24480b30b016
Maximiliano Pollinger <maximiliano@qualabs.com>
2023-03-08 13:51:07 -0300

    (fix): updated version of videojs

--- src/plugin.js
+++ src/plugin.js
@@ -1,7 +1,5 @@
 import videojs from 'video.js';
-import {version as VERSION} from '../package.json';
-
-const Plugin = videojs.getPlugin('plugin');
+import { version as VERSION } from '../package.json';
 
 // Default options for the plugin.
 const defaults = {};
@@ -11,13 +9,10 @@ const defaults = {};
  *
  * See: https://blog.videojs.com/feature-spotlight-advanced-plugins/
  */
-class Cmcd extends Plugin {
+class Cmcd {
 
   /**
    * Create a Cmcd plugin instance.
-   *
-   * @param  {Player} player
-   *         A Video.js Player instance.
    *
    * @param  {Object} [options]
    *         An optional options object.
@@ -26,20 +21,17 @@ class Cmcd extends Plugin {
    *         second argument of options is a convenient way to accept inputs
    *         from your plugin's caller.
    */
-  constructor(player, options) {
-    // the parent class will add player under this.player
-    super(player);
-
+  constructor(options) {
     this.options = videojs.mergeOptions(defaults, options);
 
-    this.player.ready(() => {
-      this.player.addClass('vjs-cmcd');
+    this.ready(() => {
+      this.addClass('vjs-cmcd');
     });
   }
 }
 
 // Define default values for the plugin's `state` object here.
-Cmcd.defaultState = {};
+// Cmcd.defaultState = {};
 
 // Include the version number.
 Cmcd.VERSION = VERSION;
