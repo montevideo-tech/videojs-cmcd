22426e812768f1013cc268663438dd4fb42a86d3
RominaSosaa <rominas@qualabs.com>
2023-03-14 15:35:18 -0300

    Change names of videos

--- listOfVideos.js
+++ listOfVideos.js
@@ -1,17 +1,17 @@
 const list = [
-    {
-        src:"https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
-        type:"application/x-mpegURL"
-    },
-    {
-        src:"https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8",
-        type:"application/x-mpegURL"
-    },
-    {
-        src:"https://moctobpltc-i.akamaihd.net/hls/live/571329/eight/playlist.m3u8",
-        type:"application/x-mpegURL"
-    }
+  {
+    src: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
+    type: 'application/x-mpegURL'
+  },
+  {
+    src: 'http://d3rlna7iyyu8wu.cloudfront.net/skip_armstrong/skip_armstrong_stereo_subs.m3u8',
+    type: 'application/x-mpegURL'
+  },
+  {
+    src: 'https://moctobpltc-i.akamaihd.net/hls/live/571329/eight/playlist.m3u8',
+    type: 'application/x-mpegURL'
+  }
 
-]
+];
 
 export default list;
