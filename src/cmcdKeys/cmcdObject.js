7be3a833a0c640719bcea3f75ba69bb41a13ba4a
coatesjuan <119955117+coatesjuan@users.noreply.github.com>
2023-03-16 12:46:38 -0300

    Add encodedBitrate, duration, topBitrate (#9)
    * merge with dev
    
    * fix instance of players and erased zombiecode

--- src/cmcdKeys/cmcdObject.js
+++ src/cmcdKeys/cmcdObject.js
@@ -4,13 +4,39 @@ export class CmcdObject {
   }
 
   getEncodedBitrate() {
-    //TODO
-    return undefined;
+    try {
+
+      //Get the bandwidth of the current quality  
+      const bandwidth = this.player.tech(true).vhs.playlists.media_.attributes.BANDWIDTH;
+
+      //Get the encodedBitrate by converting the bandwidth to kbps and rounding it to an Int
+      const encodedBitrate = Math.round(bandwidth / 1000);
+
+      return encodedBitrate;
+    }
+    catch (e) {
+      return undefined;
+    }
   }
 
-  getObjectDuration() {
-    //TODO
-    return undefined;
+  getObjectDuration(uriBeingRequested) {
+    try {
+
+      //Get the current playlist
+      const playlist = this.player.tech(true).vhs.playlists.media();
+
+      //Get the current segment index
+      const currentSegmentIndex = playlist.segments.findIndex(segment => segment.resolvedUri === uriBeingRequested);
+
+      //Get the duration of the current segment, convert it to ms and round it to an int
+      const currentSegmentDuration = Math.round(playlist.segments[currentSegmentIndex].duration * 1000);
+
+      return currentSegmentDuration;
+
+    } catch (error) {
+      return undefined;
+    }
+
   }
 
   getObjectType() {
@@ -19,14 +45,31 @@ export class CmcdObject {
   }
 
   getTopBitrate() {
-    //TODO
-    return undefined;
+    try {
+
+      //Get the list of available quality levels
+      const qualitylevels = this.player.qualityLevels().levels_;
+
+      //Get the highest bitrate
+      const highestBitrate = qualitylevels.reduce(function (prev, current) {
+        return (prev && prev.bitrate > current.bitrate) ? prev : current;
+      });
+
+      //Get the topBitrate, convert to kbps and round it
+      const topBitrate = Math.round(highestBitrate.bitrate / 1000);
+
+      return topBitrate;
+
+    } catch (error) {
+
+      return undefined;
+    }
   }
 
-  getKeys() {
+  getKeys(uriBeingRequested) {
     return {
       br: this.getEncodedBitrate(),
-      d: this.getObjectDuration(),
+      d: this.getObjectDuration(uriBeingRequested),
       ot: this.getObjectType(),
       tb: this.getTopBitrate()
     }
