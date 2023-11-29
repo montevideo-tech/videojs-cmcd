140d03625e7b31c8c0bef8a55aa365e7f8b4947c
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2023-11-29 10:34:14 -0800

    chore: update CML to 0.5.1

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -2,7 +2,13 @@ const generate = require('videojs-generate-rollup-config');
 
 // see https://github.com/videojs/videojs-generate-rollup-config
 // for options
-const options = {};
+const options = {
+  babel(dflt) {
+    dflt.plugins.push('@babel/plugin-proposal-class-properties');
+
+    return dflt;
+  }
+};
 const config = generate(options);
 
 // Add additonal builds/customization here!
